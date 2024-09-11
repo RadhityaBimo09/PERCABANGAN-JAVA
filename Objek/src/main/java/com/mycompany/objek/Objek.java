eb7463e04931da6edb5d8590a013f26a45496408
RadhityaBimo09 <bimosuhadi09@gmail.com>
2024-09-11 09:09:08 +0700

    Add files via upload

---
+++ Objek/src/main/java/com/mycompany/objek/Objek.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.objek;
+
+/**
+ *
+ * @author User
+ */
+public class Objek {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
